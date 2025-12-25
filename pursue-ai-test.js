b6792caf1ca7b5c350bac30a12ad616aed234516
cmoyates <cmoyates@users.noreply.github.com>
2025-12-25 18:21:44 +0000

    deploy: 1ffa781af3f7503d9748c3dd8d18e48a66cef86b

--- pursue-ai-test.js
+++ pursue-ai-test.js
@@ -300,28 +300,28 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
-function wasm_bindgen__convert__closures_____invoke__h9dc9282837444ba7(arg0, arg1, arg2) {
-    wasm.wasm_bindgen__convert__closures_____invoke__h9dc9282837444ba7(arg0, arg1, arg2);
-}
-
 function wasm_bindgen__convert__closures_____invoke__h1b8393f92e7239e5(arg0, arg1, arg2) {
     wasm.wasm_bindgen__convert__closures_____invoke__h1b8393f92e7239e5(arg0, arg1, arg2);
-}
-
-function wasm_bindgen__convert__closures_____invoke__hc77396fb7900a1da(arg0, arg1) {
-    wasm.wasm_bindgen__convert__closures_____invoke__hc77396fb7900a1da(arg0, arg1);
-}
-
-function wasm_bindgen__convert__closures_____invoke__hb23cd3fe1193d1af(arg0, arg1, arg2, arg3) {
-    wasm.wasm_bindgen__convert__closures_____invoke__hb23cd3fe1193d1af(arg0, arg1, arg2, arg3);
 }
 
 function wasm_bindgen__convert__closures_____invoke__he10ccb762ccf21eb(arg0, arg1) {
     wasm.wasm_bindgen__convert__closures_____invoke__he10ccb762ccf21eb(arg0, arg1);
 }
 
+function wasm_bindgen__convert__closures_____invoke__h9dc9282837444ba7(arg0, arg1, arg2) {
+    wasm.wasm_bindgen__convert__closures_____invoke__h9dc9282837444ba7(arg0, arg1, arg2);
+}
+
+function wasm_bindgen__convert__closures_____invoke__hc77396fb7900a1da(arg0, arg1) {
+    wasm.wasm_bindgen__convert__closures_____invoke__hc77396fb7900a1da(arg0, arg1);
+}
+
 function wasm_bindgen__convert__closures_____invoke__h6ba934a251c66f97(arg0, arg1, arg2) {
     wasm.wasm_bindgen__convert__closures_____invoke__h6ba934a251c66f97(arg0, arg1, isLikeNone(arg2) ? 0 : addToExternrefTable0(arg2));
+}
+
+function wasm_bindgen__convert__closures_____invoke__hb23cd3fe1193d1af(arg0, arg1, arg2, arg3) {
+    wasm.wasm_bindgen__convert__closures_____invoke__hb23cd3fe1193d1af(arg0, arg1, arg2, arg3);
 }
 
 const __wbindgen_enum_GamepadMappingType = ["", "standard"];
